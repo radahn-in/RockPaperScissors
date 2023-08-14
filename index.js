d8b978b500ffb8e78ee4500a03e0b5b595de9cff
radahn-in <raghu_nath1993@yahoo.com>
2023-08-14 19:06:24 +0530

    playRound function added

--- index.js
+++ index.js
@@ -8,9 +8,49 @@ function getComputerChoice(){
 const playerSelection = prompt("Write a Rock, Paper, or Scissors").toUpperCase();
 const computerChoice = getComputerChoice();
 
+function playRound(playerSelection, computerChoice){
+    if(playerSelection == "ROCK"){
+        if(computerChoice == "PAPER"){
+            return "Computer Won";
+        }
+        else if(computerChoice == "ROCK"){
+            return "Its a Draw!!!";
+        }
+        else if(computerChoice == "SCISSORS"){
+            return "Player Won";
+        }
+    }
+    else if(playerSelection == "SCISSORS"){
+        if(computerChoice == "PAPER"){
+            return "Player Won";
+        }
+        else if(computerChoice == "ROCK"){
+            return "Computer Won";
+        }
+        else if(computerChoice == "SCISSPRS"){
+            return "Its a Draw!!!";
+        }
+    }
+    else if(playerSelection=="PAPER"){
+        if(computerChoice == "PAPER"){
+            return "Its a Draw!!!";
+        }
+        else if(computerChoice == "ROCK"){
+            return "Player Won";
+        }
+        else if(computerChoice == "SCISSPRS"){
+            return "Computer Won";
+        }
+    }
+    
+}
+
 
 console.log("C: ",computerChoice);
 console.log("P: ",playerSelection);
+
+result = playRound(playerSelection, computerChoice);
+console.log(result);
 
 
 
